3a20c89147348d79208a077d611bb384e6804420
Tom B <tommyb603@users.noreply.github.com>
2019-10-31 09:47:51 -0400

    Update general.sh

--- general.sh
+++ general.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -x
+sudo apt-get -y update
+sudo apt-get -y install nmap
+sudo ufw allow 9090
+sudo ufw allow 9999
+wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+sudo bash -c "bash Anaconda3-5.3.0-Linux-x86_64.sh -b -p /opt/anaconda3"
+sudo bash -c "echo 'ANACONDA_HOME=/opt/anaconda3/' >> /etc/profile"
+sudo bash -c "echo 'PATH=/opt/anaconda3/bin:$PATH' >> /etc/profile"
 
-echo "This is a Apache installation script" > /tmp/silly.txt
-
-sudo apt-get update -y 
-sudo apt-get install -y apache2
-sudo systemctl restart apache2
+sudo useradd -m -s /bin/bash seed
+sudo echo "seed:dees" | chpasswd
+sudo usermod -a -G sudo seed

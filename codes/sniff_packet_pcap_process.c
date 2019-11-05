597e9b9683e5c10587e202dcb5da9abd161e6ea9
Tom B <tommyb603@users.noreply.github.com>
2019-11-05 10:36:58 -0500

    Update sniff_packet_pcap_process.c

--- codes/sniff_packet_pcap_process.c
+++ codes/sniff_packet_pcap_process.c
@@ -16,7 +16,7 @@ struct ethheader {
   u_char  ether_dhost[ETHER_ADDR_LEN]; /* destination host address */
   u_char  ether_shost[ETHER_ADDR_LEN]; /* source host address */
   u_short ether_type;                  /* IP? ARP? RARP? etc */
-}
+};
 
 /* IP Header */
 struct ipheader {
